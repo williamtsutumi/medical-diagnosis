8a62a5388df59ea95458132d14ad5a06faba8c78
williamtsutumi <william.tsutumi@gmail.com>
2024-03-02 12:25:58 -0300

    tela bonitinha

--- main.pl
+++ main.pl
@@ -13,36 +13,48 @@ main :-
     send(MainDialog, scrollbars, both),
 
     send(MainDialog, append, new(T, text('O resultado do protótipo é apenas informativo e o paciente deve\nconsultar um médico para obter um diagnóstico correto e preciso.'))),
-    send(T, font, bold),
+    send(T, colour, red),
+    send(T, font, font(arial, bold, 10)),
 
-    new(BtnCadastro, button('Cadastrar paciente')),
-        ActionCadastro = message(@prolog, cadastro,
-                                 MainDialog, '-1'),
-        send(BtnCadastro, message, ActionCadastro),
-    send(MainDialog, append, BtnCadastro),
-
-    send(MainDialog, append, new(IdSelected, text_item(id))),
+    send(MainDialog, append, new(BtnGroup, dialog_group('Selecione'))),
+    send(BtnGroup, append, new(IdSelected, text_item(id))),
     new(BtnEdit, button('Visualizar')),
         ActionEdit = message(@prolog, cadastro,
                              MainDialog,
                              IdSelected?selection),
         send(BtnEdit, message, ActionEdit),
-    send(MainDialog, append, BtnEdit),
+        send(BtnEdit, colour, dark_green),
+    send(BtnGroup, append, BtnEdit),
 
     new(BtnDiagnostico, button('Diagnosticar')),
         ActionDiagnostico = message(@prolog, on_diagnostico_click,
                                     IdSelected?selection),
         send(BtnDiagnostico, message, ActionDiagnostico),
-    send(MainDialog, append, BtnDiagnostico),
+        send(BtnDiagnostico, colour, dark_green),
+    send(BtnGroup, append, BtnDiagnostico),
 
     new(BtnDelete, button('Deletar')),
         ActionDelete = message(@prolog, on_delete_click,
                                MainDialog,
                                IdSelected?selection),
         send(BtnDelete, message, ActionDelete),
-    send(MainDialog, append, BtnDelete),
-    
-    list_patients(MainDialog),
+    send(BtnDelete, colour, red),
+    send(BtnGroup, append, BtnDelete),
+
+    new(BtnCadastro, button('Cadastrar')),
+        ActionCadastro = message(@prolog, cadastro,
+                                 MainDialog, '-1'),
+        send(BtnCadastro, message, ActionCadastro),
+        send(BtnCadastro, colour, dark_green),
+    send(MainDialog, append, BtnCadastro),
+    send(MainDialog, display, BtnCadastro, point(300, 57)),
+
+    list_patients(MainDialog, ListGroup),
+
+    new(BtnCancel, button('Sair')),
+        send(BtnCancel, message, message(@prolog, on_cancel_click, MainDialog)),
+    send(MainDialog, append, BtnCancel),
+    send(MainDialog, display, BtnCancel, point(200, ListGroup?height + ListGroup?y + 20)),
 
     send(MainDialog, open).
 
@@ -63,10 +75,8 @@ cadastro(MainDialog, IdString) :-
         send(Genero, selection, Gender)
     ; true),
 
-    send(Dialog, append, new(T, text('Sintomas'))),
-    send(T, font, bold),
-
-    send(Dialog, append, new(List, list_browser)), 
+    send(Dialog, append, new(G, dialog_group('Sintomas'))),
+    send(G, append, new(List, list_browser)), 
     forall(sintoma(S), send(List, append, dict_item(S, S))),
     send(List, multiple_selection, @on),
 
@@ -80,8 +90,18 @@ cadastro(MainDialog, IdString) :-
                                Idade?selection,
                                Genero?selection,
                                List)),
+    send(BtnSalvar, colour, dark_green),
+
+    new(BtnCancel, button('Cancelar')),
+        send(BtnCancel, message, message(@prolog, on_cancel_click, Dialog)),
+    send(Dialog, append, BtnCancel),
+
+    send(List, size, size(50, List?height)),
     send(Dialog, append, BtnSalvar),
     send(Dialog, open).
+
+on_cancel_click(Dialog) :-
+    send(Dialog, destroy).
 
 on_create_click(MainDialog, Dialog, FileName, Id, Nome, Idade, Genero, List) :-
     get(List, selection, SelectionChain),
@@ -97,12 +117,14 @@ get_item_label(Reference, Label) :-
     get(Reference, label, Label).
 
 
-list_patients(MainDialog) :-
+list_patients(MainDialog, G) :-
     retractall(patient(_,_,_,_)),
     consult('patients.txt'),
+    send(MainDialog, append, new(G, dialog_group('Pacientes'))),
     findall(patient(Id, Name, Age, Gender), patient(Id, Name, Age, Gender), Patients),
     get_patients_string(Patients, String),
-    send(MainDialog, append, new(_, text(String))).
+    send(G, append, new(_, text(String))),
+    send(MainDialog, display, G, point(0, 115)).
 
 get_patients_string([], '').
 get_patients_string([patient(Id, Nome, Idade, Genero) | R], String) :-
@@ -122,20 +144,24 @@ on_diagnostico_click(IdAtom) :-
     patient(Id, Nome, Idade, Genero),
 
     new(Dialog, dialog('Diagnostico')),
-    format(atom(PatString), 'Paciente:\nNome: ~w\nIdade: ~w\nGenero: ~w', [Nome, Idade, Genero]),
-    send(Dialog, append, new(_, text(PatString))),
+    send(Dialog, scrollbars, both),
 
+    send(Dialog, append, new(PatGroup, dialog_group('Paciente'))),
+    format(atom(PatString), 'Nome: ~w\nIdade: ~w\nGenero: ~w', [Nome, Idade, Genero]),
+    send(PatGroup, append, new(_, text(PatString))),
+
+    send(Dialog, append, new(SintGroup, dialog_group('Sintomas'))),
     findall(S, sintoma_paciente(Id, S), Sintomas),
-    send(Dialog, append, new(T, text('Sintomas:'))),
-    send(T, font, bold),
     atomic_list_concat(Sintomas, '\n', SintomaText),
-    send(Dialog, append, new(_, text(SintomaText))),
+    send(SintGroup, append, new(_, text(SintomaText))),
+    send(Dialog, display, SintGroup, point(180,76)),
 
     diagnostico(Idade, Genero, Sintomas, Result),
     format_sintomas(Result, DiagResultList),
-    atomic_list_concat(DiagResultList, '\n', Probs),
-    atomic_concat('Probabilidades (%):\n', Probs, DiagText),
-    send(Dialog, append, new(_, text(DiagText))),
+    atomic_list_concat(DiagResultList, '\n', DiagText),
+    send(Dialog, append, new(ProbGroup, dialog_group('Probabilidades (%):'))),
+    send(ProbGroup, append, new(_, text(DiagText))),
+    send(Dialog, display, ProbGroup, point(0,76)),
 
     send(Dialog, open).
 
